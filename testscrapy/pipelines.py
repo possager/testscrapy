a276ed42f582d7cab41b0fd95d6f2b521bac7081
passager <passager@163.com>
2018-03-28 05:10:44 +0800

    在pipeline中添加了判断操作系统的语句。

--- testscrapy/pipelines.py
+++ testscrapy/pipelines.py
@@ -5,14 +5,19 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
-
+import os
+import platform
 
 
 
 class TestscrapyPipeline(object):
     def process_item(self, item, spider):
         basic_file='E:/scrapy_data/'
+        if platform.system()=='Linux':
+            basic_file='/media/liang/Data/scrapy_data/'
         if type(item)==type({}):
+            if not os.path.exists(basic_file+spider.name):
+                os.makedirs(basic_file+spider.name)
 
             with open(basic_file+spider.name+'/'+item['owner'],'w+') as fl:
                 json.dump(item,fl)
